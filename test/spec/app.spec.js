78b486ba1cbf232ca66777cb7ed9f4a646c934ff
FabioMar94 <fabio.marangon94@gmail.com>
2016-08-29 09:57:16 +0200

    esame

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,23 +1,21 @@
 describe("calcolaStringa", function() {
     it("testa la stringa vuota", function() { 
-    
-        calcola('');
+
       expect( calcola('')).toBe(-1);
         
   });   
     
     it("testa una stringa con una c",function(){
-        calcola('ciao'),
             expect(calcola('ciao')).toBe(1);
     });
     
     it("testa una stringa con più c",function(){
-        calcola('ciaocc'),
+
             expect(calcola('ciaocc')).toBe(3);
     });
     
     it("testa una stringa senza c",function(){
-        calcola('iao'),
+
             expect(calcola('iao')).toBe(0);
     });
  
